ffaa68684f76ca59064ee3999da9f09704cce66f
Pat abhishek <patabhishek@Pats-MacBook-Air.local>
2022-09-08 13:40:34 -0400

    usedebounce

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, TextInput } from "react-native";
 
 export default function App() {
+  const updateDebounceText = debounce((text) => {
+    console.log(text);
+  });
+  function debounce(cb, delay = 500) {
+    let timeout;
+    return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        cb(...args);
+      }, delay);
+    };
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        placeholder="Edit me"
+        onChangeText={(text) => updateDebounceText(text)}
+      ></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
